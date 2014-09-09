1b4f9562e98a1e395a818d173b19992dc0fc2743
Adam Vega <adam.j.vega@hotmail.com>
2014-09-08 20:14:48 -0500

    initial commit

--- High_Low_Game.php
+++ High_Low_Game.php
@@ -18,36 +18,75 @@
 //    Use exit(0) to end the game
 //    If you get stuck in game, ctrl-c will exit.
 
+//============Prior iteration of game==============
+// echo "Welcome to High Low Game!\n";
+
+// $game_num=rand ($argv[1] , $argv[2]);
+// $play_game=true;
+// $num_guesses=0;
+
+// //echo $game_num;
+// fwrite(STDOUT, "Input an integer. ");
+
+// $int_guess = fgets(STDIN);
+
+// while($play_game){	
+// 	if($int_guess<$game_num){
+// 		echo "HIGHER \n";
+// 		fwrite(STDOUT, "Input an integer. ");
+
+// 		$int_guess = fgets(STDIN);
+// 		++$num_guesses;
+// 	}elseif($int_guess>$game_num){
+// 		echo "Lower\n";
+// 		fwrite(STDOUT, "Input an integer. ");
+
+// 		++$num_guesses;
+// 		$int_guess = fgets(STDIN);
+// 	}elseif($int_guess == $game_num){
+// 		echo "Good Guess!\n";
+// 		$play_game = false;
+// 		++$num_guesses;
+//     }
+// }
+// echo "You guessed " . $num_guesses . " times\n";
+
+//============Final iteration of game===============
+
 echo "Welcome to High Low Game!\n";
-$game_num=rand (1 , 100);
-$Game_End=10;
-//echo $game_num;
-		fwrite(STDOUT, "Input an integer between 1 and 100. ");
-		$Integer_guess = fgets(STDIN);
-while($Game_End==10){	
-	if($Integer_guess<$game_num){
-	echo "HIGHER \n";
-	fwrite(STDOUT, "Input an integer between 1 and 100. ");
-	$Integer_guess = fgets(STDIN);
-	}elseif($Integer_guess>$game_num){
-	echo "Lower\n";
-	fwrite(STDOUT, "Input an integer between 1 and 100. ");
-	$Integer_guess = fgets(STDIN);
-	}elseif($Integer_guess == $game_num){
-	echo "Good Guess!";
-	--$Game_End;
-    }
-}    
+if(argc)//finish this later to check if argc has two values, if not make the array default to 1 to 100
+$game_num=rand($argv[1],$argv[2]);
+$int_guess=0;
+$attempts=1;
+$play_again="yes";
+//End variables
+
+//Do Loop to get user input and reassign $int_guess value
+while($play_again=="yes"){
+	fwrite(STDOUT, "Input an integer. ");
+
+	do
+	{	
+		$int_guess = fgets(STDIN);
+
+		if($int_guess > $game_num) 
+		{
+			fwrite(STDOUT, "Lower\n");
+			++$attempts;
+		} 
+		elseif($int_guess < $game_num) 
+		{
+			fwrite(STDOUT, "Greater\n");
+			++$attempts;
+		}
+	} 
+	while($int_guess != $game_num );
+		echo "You win! You guessed " . $attempts . " times." . "\n";
+	
+	fwrite(STDOUT, "Would you like to play again? Enter yes or no");
+	$play_again = fgets(STDIN);
+	fwrite(STDOUT, "Input an integer. ");
+
+} 
+while ($play_again = "yes");
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
